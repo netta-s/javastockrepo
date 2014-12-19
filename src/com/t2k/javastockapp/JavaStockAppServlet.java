345b207c7534136278be1938b3e97c92518b419a
netta-s <netta.sulema@gmail.com>
2014-12-19 13:47:45 +0200

    Exercise 2: calculate numbers and show on page.

--- src/com/t2k/javastockapp/JavaStockAppServlet.java
+++ src/com/t2k/javastockapp/JavaStockAppServlet.java
@@ -7,7 +7,16 @@ import javax.servlet.http.*;
 public class JavaStockAppServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		
+		int num1, num2, num3;
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		
+		int result = num3 * (num1 + num2);
+		String resultStr = new String("<h1>Result of " + num3 + " * (" + num1 + " + " + num2 + ") = " + result + "</h1>");
+		
+		resp.getWriter().println(resultStr);
 	}
 }
